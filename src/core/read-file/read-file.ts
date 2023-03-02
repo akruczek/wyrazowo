44cfd0cf2394f52ebbf964989ebdbec7667dfc9c
Adam Kruczek <a.kruczek@fibargroup.com>
2023-03-02 18:45:49 +0100

    v0.1.1 words database security improvements

--- src/core/read-file/read-file.ts
+++ src/core/read-file/read-file.ts
@@ -1,5 +1,5 @@
 // TODO: implement read from file
-import { slowa } from '../../assets/slowa'
+import slowa from '../../assets/slowa'
 
 export const asyncReadFile = async (filename: string, type?: string) => new Promise<string[]>((resolve, reject) => {
   resolve(slowa.split('.'))
